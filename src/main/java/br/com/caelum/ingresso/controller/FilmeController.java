4854320303719567ce238a5dda680a6b1cb12026
Patrick Mendes Ferreira <pmendesf@me.com>
2017-09-23 16:42:52 -0300

    Criando a tela para selecao de lugares e colocando uma Capa do filme

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -99,7 +99,7 @@ public class FilmeController {
 
 		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
 
-		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme, DetalhesDoFilme.class);
 
 		modelAndView.addObject("sessoes", sessoes);
 		modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
